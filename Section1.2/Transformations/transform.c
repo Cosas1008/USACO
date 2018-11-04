44658d0954d75b985391ea60f03564ab3080260e
yuweichen1008 <yuweiichen@gmail.com>
2018-11-04 13:10:13 +0800

    undone transform

--- Section1.2/Transformations/transform.c
+++ Section1.2/Transformations/transform.c
@@ -6,18 +6,33 @@ TASK: transform
 
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 
 int main(void)
 {
     FILE *fin, *fout;
     fin = fopen("transform.in","r");
-    
-
-
-    fout = fopen("transform.out","w");
-
-
+    int n;
+    char c;
+    fscanf(fin,"%d",&n);
+    char *matrix = malloc(n*n*sizeof(char));
+    memset(matrix,0, sizeof(char)*n*n);
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j < n; j++){
+            c = fgetc(fin);
+            if(c == '@' || c == '-'){
+                *(matrix + i*n + j) = c;
+            }
+        }
+    }
     fclose(fin);
+    fout = fopen("transform.out","w");
+    for(int i = 0; i < n; i++){
+        for(int j =0; j < n; j++){
+            printf("%c",*(matrix + i*n + j));
+        }
+        printf("\n");
+    }
     fclose(fout);
     return 0;
 }
