430d58a3ecc353f23d69ec291f4d9ed459d2f3be
yuweichen1008 <yuweiichen@gmail.com>
2018-11-06 14:46:31 +0800

    Too Complicated Implement for mem operation

--- Section1.2/Transformations/transform.c
+++ Section1.2/Transformations/transform.c
@@ -8,33 +8,52 @@ TASK: transform
 #include <stdio.h>
 #include <string.h>
 
+#define MAX_BUFF 255
 
 int main(void)
 {
     FILE *fin, *fout;
-    fin = fopen("transform.in","r");
     int n;
     char c;
+    char *ma, *mb,*mc, *buff;    
+    fin = fopen("transform.in","r");
+    if (fin == NULL) {
+        printf("Error: file pointer is null.");
+        exit(1);
+    }
+
     fscanf(fin,"%d",&n);
-    char *ma = malloc(n*n*sizeof(char));
+    printf("%d\n", n);
+    ma = malloc(n*n*sizeof(char));
+    mb = malloc(n*n*sizeof(char));
+    mc = malloc(n*n*sizeof(char));
+    buff = malloc(MAX_BUFF*sizeof(char));
+
     memset(ma,0, sizeof(char)*n*n);
-    for(int i = 0; i < n; i++){
-        for(int j = 0; j < n; j++){
-            c = fgetc(fin);
-            if(c == '@' || c == '-'){
-                *(ma + i*n + j) = c;
-            }
-        }
+    memset(mb,0, sizeof(char)*n*n);
+    memset(mc,0, sizeof(char)*n*n);
+    memset(buff,0,sizeof(char)*MAX_BUFF);
+
+    printf("A:\n");
+    for(int i=0; i<n; i++){
+        fgets(buff,MAX_BUFF,(FILE*)fin);
+        printf("%s\n", buff);
+        memcpy(ma+i*n,buff,n);
     }
-    fclose(fin);
+    printf("B:\n");
+    for(int i=0; i<n; i++){
+        fgets(buff,MAX_BUFF,(FILE*)fin);
+        printf("%s\n", buff);
+        memcpy(mb+i*n,buff,n);
+    }
+    // done reading
     fout = fopen("transform.out","w");
-    for(int i = 0; i < n; i++){
-        for(int j =0; j < n; j++){
-            printf("%c",*(ma + i*n + j));
-        }
-        printf("\n");
-    }
+    printf("%s",mb);
     free(ma);
+    free(mb);
+    free(mc);
+    free(buff);
+    fclose(fin);
     fclose(fout);
     return 0;
 }
