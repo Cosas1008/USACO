01a8ee31a9e554fc30be6507ba49e18232d66b8c
yuweichen1008 <yuweiichen@gmail.com>
2018-11-08 21:15:22 +0800

    update tramsform

--- Section1.2/Transformations/transform.c
+++ Section1.2/Transformations/transform.c
@@ -4,55 +4,148 @@ LANG: C
 TASK: transform
 */
 
-#include <stdlib.h>
 #include <stdio.h>
-#include <string.h>
+#include <stdlib.h>
 
-#define MAX_BUFF 255
-
-int main(void)
+int n;
+int Equal(char **a, char **b)
 {
+    char *tar1, *tar2;
+    int i, j;
+    for (i = 1; i <= n; i++)
+    {
+        tar1 = (char *)(a + i);
+        tar2 = (char *)(b + i);
+        for (j = 1; j <= n; j++)
+        {
+            if (tar1[j] != tar2[j])
+                return 0;
+        }
+    }
+    return 1;
+}
+void r90(char **transformed, char **original)
+{
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            transformed[j][n - i - 1] = original[i][j];
+        }
+    }
+}
+
+void filp(char **transformed, char **original)
+{
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            transformed[i][n - j - 1] = original[i][j];
+        }
+    }
+}
+int main()
+{
+    int i, j, k, identical = 0, opt = 0;
+    char **ori, **tag, **tmp1, **tmp2; // input original and target pattern
+                                       // char ** tmp, ** ma, ** mb;
     FILE *fin, *fout;
-    int n;
-    char c;
-    char *ma, *mb,*mc, *buff;    
-    fin = fopen("transform.in","r");
-    if (fin == NULL) {
-        printf("Error: file pointer is null.");
-        exit(1);
+
+    // Read File
+    fin = fopen("transform.in", "r");
+    fscanf(fin, "%d", &n);
+
+    // memory allocation
+    ori = (char **)malloc(n * sizeof(char));
+    tag = (char **)malloc(n * sizeof(char));
+    tmp1 = (char **)malloc(n * sizeof(char));
+    tmp2 = (char **)malloc(n * sizeof(char));
+    for (i = 0; i < n; i++)
+    {
+        ori[i] = (char *)malloc(n * sizeof(char));
+        tag[i] = (char *)malloc(n * sizeof(char));
+        tmp1[i] = (char *)malloc(n * sizeof(char));
+        tmp2[i] = (char *)malloc(n * sizeof(char));
     }
 
-    fscanf(fin,"%d",&n);
-    printf("%d\n", n);
-    ma = malloc(n*n*sizeof(char));
-    mb = malloc(n*n*sizeof(char));
-    mc = malloc(n*n*sizeof(char));
-    buff = malloc(MAX_BUFF*sizeof(char));
-
-    memset(ma,0, sizeof(char)*n*n);
-    memset(mb,0, sizeof(char)*n*n);
-    memset(mc,0, sizeof(char)*n*n);
-    memset(buff,0,sizeof(char)*MAX_BUFF);
-
-    printf("A:\n");
-    for(int i=0; i<n; i++){
-        fgets(buff,MAX_BUFF,(FILE*)fin);
-        printf("%s\n", buff);
-        memcpy(ma+i*n,buff,n);
+    // load data
+    for (i = 0; i < n; i++)
+    {
+        fscanf(fin, "%s", &ori[i][1]);
     }
-    printf("B:\n");
-    for(int i=0; i<n; i++){
-        fgets(buff,MAX_BUFF,(FILE*)fin);
-        printf("%s\n", buff);
-        memcpy(mb+i*n,buff,n);
+    for (i = 0; i < n; i++)
+    {
+        fscanf(fin, "%s", &tag[i][1]);
     }
-    // done reading
-    fout = fopen("transform.out","w");
-    printf("%s",mb);
-    free(ma);
-    free(mb);
-    free(mc);
-    free(buff);
+
+    //check identical
+    //90   cw opt:1
+    r90(tmp1, ori);
+    if (Equal(tmp1, tag))
+        opt = 1;
+    else
+    { //180  cw opt:2
+        r90(tmp2, tmp1);
+        if (Equal(tmp2, tag))
+            opt = 2;
+        else
+        { //270  cw opt:3
+            r90(tmp1, tmp2);
+            if (Equal(tmp1, tag))
+                opt = 3;
+            else
+            { //nochange  opt:6
+                if (Equal(ori, tag))
+                    opt = 6;
+                else
+                { // flip opt:4
+                    filp(tmp1, ori);
+                    if (Equal(tmp1, tag))
+                        opt = 4;
+                    else
+                    {
+                        //270  fcw opt:5
+                        filp(tmp2, tmp1);
+                        if (Equal(tmp2, tag))
+                            opt = 5;
+                        else
+                        { //90  fcw opt:5
+                            r90(tmp1, ori);
+                            filp(tmp2, tmp1);
+                            if (Equal(tmp2, tag))
+                                opt = 5;
+                            else
+                            { //180 fcw opt:5
+                                r90(tmp1, tmp2);
+                                if (Equal(tmp1, tag))
+                                    opt = 5;
+                                else
+                                { // invalid transform
+                                    opt = 7;
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+    //output file
+    fout = fopen("transform.out", "w");
+    fprintf(fout, "%d", opt);
+    // Free dynamic allocated memory
+    for (i = 0; i < n; i++)
+    {
+        free(ori[i]);
+        free(tag[i]);
+        free(tmp[i]);
+    }
+
+    free(ori);
+    free(tag);
+    free(tmp);
+    // Close file
     fclose(fin);
     fclose(fout);
     return 0;
