c92be88676cb41aed315dfa2477c8db8f68a91c0
yuweichen1008 <yuweiichen@gmail.com>
2018-11-10 00:25:12 +0800

    transform malfunction

--- Section1.2/Transformations/transform.c
+++ Section1.2/Transformations/transform.c
@@ -12,11 +12,11 @@ int Equal(char **a, char **b)
 {
     char *tar1, *tar2;
     int i, j;
-    for (i = 1; i <= n; i++)
+    for (i = 0; i < n; i++)
     {
         tar1 = (char *)(a + i);
         tar2 = (char *)(b + i);
-        for (j = 1; j <= n; j++)
+        for (j = 0; j < n; j++)
         {
             if (tar1[j] != tar2[j])
                 return 0;
@@ -45,11 +45,11 @@ void filp(char **transformed, char **original)
         }
     }
 }
+
 int main()
 {
     int i, j, k, identical = 0, opt = 0;
-    char **ori, **tag, **tmp1, **tmp2; // input original and target pattern
-                                       // char ** tmp, ** ma, ** mb;
+    char **ori, **tag, **tmp1, **tmp2; // 2d char array
     FILE *fin, *fout;
 
     // Read File
@@ -57,10 +57,11 @@ int main()
     fscanf(fin, "%d", &n);
 
     // memory allocation
-    ori = (char **)malloc(n * sizeof(char));
-    tag = (char **)malloc(n * sizeof(char));
-    tmp1 = (char **)malloc(n * sizeof(char));
-    tmp2 = (char **)malloc(n * sizeof(char));
+    // modefied sizeof(char) to sizeof(char*)
+    ori = (char **)malloc(n * sizeof(char*));
+    tag = (char **)malloc(n * sizeof(char*));
+    tmp1 = (char **)malloc(n * sizeof(char*));
+    tmp2 = (char **)malloc(n * sizeof(char*));
     for (i = 0; i < n; i++)
     {
         ori[i] = (char *)malloc(n * sizeof(char));
@@ -72,11 +73,12 @@ int main()
     // load data
     for (i = 0; i < n; i++)
     {
-        fscanf(fin, "%s", &ori[i][1]);
+        fscanf(fin, "%s", &ori[i][0]);
+        // printf("%c%c%c%c\n",ori[i][0], ori[i][1], ori[i][2], ori[i][3]); // n = 4
     }
     for (i = 0; i < n; i++)
     {
-        fscanf(fin, "%s", &tag[i][1]);
+        fscanf(fin, "%s", &tag[i][0]);
     }
 
     //check identical
@@ -104,24 +106,22 @@ int main()
                     if (Equal(tmp1, tag))
                         opt = 4;
                     else
-                    {
-                        //270  fcw opt:5
-                        filp(tmp2, tmp1);
+                    { //90  fcw opt:5
+                        r90(tmp2, tmp1);
                         if (Equal(tmp2, tag))
                             opt = 5;
                         else
-                        { //90  fcw opt:5
-                            r90(tmp1, ori);
-                            filp(tmp2, tmp1);
-                            if (Equal(tmp2, tag))
+                        { //180 fcw opt:5
+                            r90(tmp1, tmp2);
+                            if (Equal(tmp1, tag))
                                 opt = 5;
                             else
-                            { //180 fcw opt:5
-                                r90(tmp1, tmp2);
-                                if (Equal(tmp1, tag))
+                            { //270  fcw opt:5
+                                r90(tmp2,tmp1);
+                                if(Equal(tmp2, tag))
                                     opt = 5;
                                 else
-                                { // invalid transform
+                                {  // invalid transform
                                     opt = 7;
                                 }
                             }
@@ -131,20 +131,23 @@ int main()
             }
         }
     }
+    
     //output file
     fout = fopen("transform.out", "w");
-    fprintf(fout, "%d", opt);
+    fprintf(fout, "%d\n", opt);
     // Free dynamic allocated memory
     for (i = 0; i < n; i++)
     {
         free(ori[i]);
         free(tag[i]);
-        free(tmp[i]);
+        free(tmp1[i]);
+        free(tmp2[i]);
     }
 
     free(ori);
     free(tag);
-    free(tmp);
+    free(tmp1);
+    free(tmp2);
     // Close file
     fclose(fin);
     fclose(fout);
